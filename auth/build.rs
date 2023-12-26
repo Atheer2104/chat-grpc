257c40195809cfb28d0a8b897ee529bc2876fb7c
Atheer <atheerwaled66@hotmail.se>
2023-12-26 12:14:45 +0100

    add proto & build script & necessary dependencies
    the proto folder contains the protocol buffer definition for auth service
    the build script is used to auto generate the code from proto using tonic build

---
+++ auth/build.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let proto_file = "./proto/auth.proto";
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    tonic_build::configure()
+        // this will generate the client code for gRPC
+        .build_client(true)
+        // this till generate the server code for gRPC
+        .build_server(true)
+        // this will set the path for the path that contains encoded prost types, this is required for
+        // implementing gRPC server reflection
+        .file_descriptor_set_path(out_dir.join("store_descriptor.bin"))
+        // set the output directory for the generate code
+        .out_dir("src")
+        // first argumetn is for the files we want to compile, second argumeent is the root location for these files
+        .compile(&[proto_file], &["proto"])?;
+
+    Ok(())
+}
