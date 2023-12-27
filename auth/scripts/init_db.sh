63406addf401247e3016efe1191380d1255c18f4
Atheer <atheerwaled66@hotmail.se>
2023-12-27 12:23:26 +0100

    add migration run in script

--- auth/scripts/init_db.sh
+++ auth/scripts/init_db.sh
@@ -60,7 +60,7 @@ if [[ -z "${SKIP_DOCKER}" ]]; then
       -e POSTGRES_DB=${DB_NAME} \
       -p "${DB_PORT}":5432 \
       -d \
-      --name "postgres" \
+      --name "postgres_$(date '+%s')" \
       postgres -N 1000
       # ^ Increased maximum number of connections for testing purposes
 fi
@@ -75,6 +75,6 @@ done
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 sqlx database create
-#sqlx migrate run
+sqlx migrate run
 
 >&2 echo "Postgres has been migrated, ready to go!"
