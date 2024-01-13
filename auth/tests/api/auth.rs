fe613d83acc3c525792c2aba33f15f84ad2531cf
Atheer <atheerwaled66@hotmail.se>
2024-01-13 20:00:48 +0100

    Make integration test work
    made the integration work with our new error implementation, in login it looked at the returned message not anymore, and in register we had to change the password since minimum requirment length is 8, which previously it did not fulfill.

--- auth/tests/api/auth.rs
+++ auth/tests/api/auth.rs
@@ -13,12 +13,9 @@ async fn missing_field_in_login_request() {
     // the servcie is ready and then we can start to make the request
     tokio::time::sleep(Duration::from_millis(100)).await;
 
-    let test_cases = vec![
-        ("", "password", "missing username field"),
-        ("user", "", "missing password field"),
-    ];
+    let test_cases = vec![("", "password"), ("user", "")];
 
-    for (username, password, error_message) in test_cases {
+    for (username, password) in test_cases {
         let response = app
             .login(Request::new(LoginRequest {
                 username: username.into(),
@@ -26,8 +23,7 @@ async fn missing_field_in_login_request() {
             }))
             .await;
 
-        assert!(response
-            .is_err_and(|x| x.code() == Code::InvalidArgument && x.message() == error_message))
+        assert!(response.is_err_and(|x| x.code() == Code::InvalidArgument))
     }
 }
 
@@ -43,7 +39,7 @@ async fn register_user_dont_check_token() {
             lastname: "ABC".into(),
             username: "atheer2104".into(),
             email: "atheer@gmail.com".into(),
-            password: "testing".into(),
+            password: "strong password".into(),
         }))
         .await;
 
@@ -58,5 +54,5 @@ async fn register_user_dont_check_token() {
     assert_eq!(saved.lastname, "ABC");
     assert_eq!(saved.username, "atheer2104");
     assert_eq!(saved.email, "atheer@gmail.com");
-    assert_eq!(saved.password, "testing");
+    assert_eq!(saved.password, "strong password");
 }
