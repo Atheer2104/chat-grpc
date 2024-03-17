1fc6ca9099cb6ebc12ab3485a172516d9e522de7
Atheer <atheerwaled66@hotmail.se>
2024-03-17 17:03:27 +0100

    feat: update login to utilize password hash

--- auth/src/server/auth_service/login.rs
+++ auth/src/server/auth_service/login.rs
@@ -1,7 +1,11 @@
+use secrecy::Secret;
 use sqlx::PgPool;
 use thiserror::Error;
+use tokio::task::spawn_blocking;
 
 use crate::proto::auth::LoginRequest;
+
+use super::verify_password_hash;
 
 #[derive(Debug, Error)]
 pub enum CheckUserExistsError {
@@ -9,26 +13,57 @@ pub enum CheckUserExistsError {
     NonExistingUser,
     #[error("Something went wrong in the DB: {0}")]
     DatabaseError(#[from] sqlx::Error),
+    #[error(transparent)]
+    UnexpectedError(#[from] tokio::task::JoinError),
+}
+
+#[tracing::instrument(name = "Get stored password hash", skip(username, db_pool))]
+pub async fn get_stored_password_hash(
+    username: &str,
+    db_pool: &PgPool,
+) -> Result<Option<(i32, Secret<String>)>, sqlx::Error> {
+    let query = sqlx::query!(
+        r#"SELECT user_id, password_hash from account WHERE username = $1"#,
+        username
+    )
+    .fetch_optional(db_pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to retrieve password hash: {:?}", e);
+        e
+    })?
+    .map(|row| (row.user_id, Secret::new(row.password_hash)));
+
+    Ok(query)
 }
 
 pub async fn check_user_exists(
     login_request: LoginRequest,
     db_pool: &PgPool,
 ) -> Result<(), CheckUserExistsError> {
-    let query = sqlx::query!(
-        r#"SELECT username, password FROM account
-       WHERE username = $1 AND password = $2"#,
-        login_request.username,
-        login_request.password
-    )
-    .fetch_optional(db_pool)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to exectute query {:?}", e);
-        e
-    })?;
+    let mut user_id = None;
+    // some dummy value
+    let mut expected_password_hash = Secret::new(
+        "$argon2id$v=19$m=15000,t=2,p=1$\
+            gZiV/M1gPc22ElAH/Jh1Hw$\
+            CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno"
+            .to_string(),
+    );
 
-    match query {
+    if let Some((stored_user_id, stored_password_hash)) =
+        get_stored_password_hash(login_request.username.as_ref(), db_pool).await?
+    {
+        user_id = Some(stored_user_id);
+        expected_password_hash = stored_password_hash;
+    }
+
+    let _ = spawn_blocking(move || {
+        verify_password_hash(expected_password_hash, login_request.password)
+    })
+    .await
+    .map_err(CheckUserExistsError::UnexpectedError);
+
+    match user_id {
         // the user was successfully added
         Some(_) => Ok(()),
         None => Err(CheckUserExistsError::NonExistingUser),
