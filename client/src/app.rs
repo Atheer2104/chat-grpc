71db8f0f1367a0c764bbbc5309dcd50afcdab7ff
Atheer <atheerwaled66@hotmail.se>
2024-07-26 22:11:36 +0200

    feat: add home and footer component to app

--- client/src/app.rs
+++ client/src/app.rs
@@ -1,11 +1,34 @@
-#[derive(Debug)]
+use crate::components::{Footer, Home};
+
+#[derive(PartialEq)]
+pub enum AppMode {
+    View,
+    Write,
+}
+
+#[derive(PartialEq)]
+pub enum AppView {
+    Home,
+    Chat,
+}
+
 pub struct App {
     pub should_quit: bool,
+    pub view: AppView,
+    pub mode: AppMode,
+    pub home: Home,
+    pub footer: Footer,
 }
 
 impl Default for App {
     fn default() -> App {
-        Self { should_quit: false }
+        Self {
+            should_quit: false,
+            view: AppView::Home,
+            mode: AppMode::View,
+            home: Home::new(),
+            footer: Footer::new(),
+        }
     }
 }
 
@@ -16,5 +39,12 @@ impl App {
 
     pub fn exit(&mut self) {
         self.should_quit = true
+    }
+
+    pub fn toggle_mode(&mut self) {
+        match self.mode {
+            AppMode::View => self.mode = AppMode::Write,
+            AppMode::Write => self.mode = AppMode::View,
+        }
     }
 }
