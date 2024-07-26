e6501e05999755704e69f1e039d973186b81f4be
Atheer <atheerwaled66@hotmail.se>
2024-07-26 22:10:33 +0200

    feat: add new events to handle

--- client/src/events/event.rs
+++ client/src/events/event.rs
@@ -3,16 +3,31 @@ use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 use crate::app::App;
 
 pub async fn action(app: &mut App, key_event: KeyEvent) {
-    match key_event.code {
-        KeyCode::Char('q') => {
-            app.exit();
-        }
-        KeyCode::Char('c') | KeyCode::Char('C') => {
-            if key_event.modifiers == KeyModifiers::CONTROL {
-                app.exit()
+    match app.mode {
+        crate::app::AppMode::View => match key_event.code {
+            KeyCode::Char('q') => {
+                app.exit();
             }
-        }
-        // for key events which we don't care about
-        _ => {}
+            KeyCode::Char('c') | KeyCode::Char('C') => {
+                if key_event.modifiers == KeyModifiers::CONTROL {
+                    app.exit()
+                }
+            }
+            KeyCode::Char('w') => app.toggle_mode(),
+            KeyCode::Char('j') | KeyCode::Down => app.home.select_next(),
+            KeyCode::Char('k') | KeyCode::Up => app.home.select_previous(),
+            KeyCode::Enter => app.home.select(),
+            // for key events which we don't care about
+            _ => {}
+        },
+        crate::app::AppMode::Write => match key_event.code {
+            KeyCode::Esc => app.toggle_mode(),
+            KeyCode::Char('c') | KeyCode::Char('C') => {
+                if key_event.modifiers == KeyModifiers::CONTROL {
+                    app.exit()
+                }
+            }
+            _ => {}
+        },
     }
 }
