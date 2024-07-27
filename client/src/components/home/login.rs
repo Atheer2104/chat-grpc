3519733c76e28a2daecf159a6297d749016e0470
Atheer <atheerwaled66@hotmail.se>
2024-07-27 22:16:55 +0200

    feat: add helper text for login popup

--- client/src/components/home/login.rs
+++ client/src/components/home/login.rs
@@ -1,11 +1,9 @@
-use std::default;
-
 use crossterm::event::KeyEvent;
 use ratatui::{
     layout::{Constraint, Direction, Layout, Rect},
-    style::Stylize,
-    symbols::block::HALF,
-    widgets::{Block, BorderType, Clear, Padding},
+    style::{Style, Stylize},
+    text::{Line, Span},
+    widgets::{Block, BorderType, Clear, Padding, Paragraph, Wrap},
     Frame,
 };
 use tui_prompts::{FocusState, Prompt, State, TextPrompt, TextRenderStyle, TextState};
@@ -112,16 +110,18 @@ impl<'a> Login<'a> {
     pub fn render(&mut self, frame: &mut Frame, area: Rect) {
         let login_block = Block::bordered()
             .border_type(BorderType::Rounded)
-            .padding(Padding::horizontal(5))
+            .padding(Padding::horizontal(2))
             .title("Login Form".bold().into_centered_line());
 
-        let block_area = centered_rect(40, 10, area);
+        let block_area = centered_rect(45, 25, area);
 
         let layout = Layout::default()
             .direction(Direction::Vertical)
             .constraints([
-                Constraint::Percentage(25),
-                Constraint::Length(2),
+                Constraint::Percentage(15),
+                Constraint::Length(1),
+                Constraint::Length(4),
+                Constraint::Length(1),
                 Constraint::Length(2),
             ])
             .split(login_block.inner(block_area));
@@ -132,8 +132,33 @@ impl<'a> Login<'a> {
 
         TextPrompt::from("Username").draw(frame, layout[1], &mut self.username_state);
 
+        let username_helper_text = vec![
+            Line::from(Span::styled("Maximum of 255 character", Style::default())),
+            Line::from(Span::styled(
+                "Following charcters are forbidden",
+                Style::default(),
+            )),
+            Line::from(Span::styled(
+                format!(
+                    "{:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}, {:?}",
+                    '/', '(', ')', '"', '<', '>', '\\', '{', '}'
+                ),
+                Style::default().red(),
+            )),
+        ];
+
+        let username_helper_paragraph = Paragraph::new(username_helper_text);
+        frame.render_widget(username_helper_paragraph, layout[2]);
+
         TextPrompt::from("Password")
             .with_render_style(TextRenderStyle::Password)
-            .draw(frame, layout[2], &mut self.password_state);
+            .draw(frame, layout[3], &mut self.password_state);
+
+        let password_helper_text = vec![
+            Line::from(Span::styled("Minimum of 8 character", Style::default())),
+            Line::from(Span::styled("Maximum of 255 character", Style::default())),
+        ];
+        let password_helper_paragraph = Paragraph::new(password_helper_text);
+        frame.render_widget(password_helper_paragraph, layout[4]);
     }
 }
